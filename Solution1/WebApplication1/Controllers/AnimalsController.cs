96fc173c365b3ec93ac73a1e6ffaa1752e5a913e
Mateusz Wdzienkowski <mateuszwdzienkowski@gmail.com>
2024-04-05 13:12:35 +0200

    Added controller, endpoint, model

---
+++ Solution1/WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase 
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAnimal(int id)
+    {
+        return Ok();
+    }
+}
---
+++ Solution1/WebApplication1/Endpoints/AnimalEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using WebApplication1.Models;
+
+namespace WebApplication1.Endpoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalEndpoints(this WebApplication app)
+    {
+        /*
+        app.MapPost("/animals", (Animal animal, int id) =>
+        {
+            
+        })*/
+    }
+}
